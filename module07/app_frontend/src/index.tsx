4f07f5a8c078985ddbe01ab5d03bee2f5ca6e7fd
rafawessling <rafaoening@gmail.com>
2024-02-12 22:21:21 +0100

    Added initial frontend project with typescript

---
+++ module07/app_frontend/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Main from './pages/main';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <React.StrictMode>
+        <Main />
+    </React.StrictMode>
+);
---
+++ module07/app_frontend/src/pages/main/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Main() {
+    return (
+        <div className="container-main">
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default Main;
